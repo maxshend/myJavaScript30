ac333bdd05869a078b6324af91d96828286c7c52
maxshend <shendrik.mv@gmail.com>
2020-11-24 00:58:56 +0300

    Finish Countdown Timer.

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let countdown
+  const timerDisplay = document.querySelector('.display__time-left')
+  const endTime = document.querySelector('.display__end-time')
+  const buttons = document.querySelectorAll('[data-time]')
+
+  buttons.forEach(button => {
+    button.addEventListener('click', startTimer)
+  })
+
+  function startTimer() {
+    timer(parseInt(this.dataset.time))
+  }
+
+  document.customForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    timer(this.minutes.value * 60)
+    this.reset()
+  })
+
+  function timer(seconds) {
+    clearInterval(countdown)
+
+    const now = Date.now()
+    const then = now + seconds * 1000
+    displayTimeLeft(seconds)
+    displayEndTime(then)
+
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000)
+      if (secondsLeft < 0) {
+        clearInterval(countdown)
+        return
+      }
+  
+      displayTimeLeft(secondsLeft)
+    }, 1000)
+  }
+
+  function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60)
+    seconds %= 60
+    const display = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+
+    timerDisplay.textContent = display
+    document.title = display
+  }
+
+  function displayEndTime(timestamp) {
+    const end = new Date(timestamp)
+    const hour = end.getHours()
+    const minutes = end.getMinutes()
+
+    endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`
+  }
+})
